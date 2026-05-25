61d95132e45e7df9b83f867e66e2c2570f825268
hansutapak <gamrikgaaling8@gmail.com>
2026-05-25 21:05:46 +0100

    updates on get_data

--- get_data.py
+++ get_data.py
@@ -66,4 +66,3 @@ if not os.path.exists('data'):
 # Save to CSV
 df.to_csv('data/paris_weather.csv', index=False)
 print("Data saved to data/paris_weather.csv")
-print("Data saved to data/paris_weather.csv")
